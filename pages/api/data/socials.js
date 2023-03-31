28ed8ea1e87dd5d3a1bf93a617d670843588c45f
Farid0558475484 <faridmh@code.edu.az>
2023-03-31 17:40:26 +0400

    feat:API Creation

---
+++ pages/api/data/socials.js
@@ -0,0 +1,27 @@
+export const socials = [
+  {
+    id: 1,
+    icon: "youtube",
+    path: "https://github.com/Farid0558475484",
+  },
+  {
+    id: 2,
+    icon: "instagram",
+    path: "https://github.com/Farid0558475484",
+  },
+  {
+    id: 3,
+    icon: "linkedin",
+    path: "https://www.linkedin.com/in/farid-hacizade-bab228167/",
+  },
+  {
+    id: 4,
+    icon: "vk",
+    path: "https://github.com/Farid0558475484",
+  },
+  {
+    id: 5,
+    icon: "twitter",
+    path: "https://github.com/Farid0558475484",
+  },
+];
